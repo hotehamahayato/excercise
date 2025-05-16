bc3ba6ae015570dfa65b496029bef55846c9ea41
hotehamahayato <hayato.hotehama@nds-osk.co.jp>
2025-05-16 09:47:26 +0900

    commit ex5_1

---
+++ Python_Ex/ex5_1.py
@@ -0,0 +1,18 @@
+# 1
+a = 3
+b = 4
+
+# 2
+print(a*b)
+
+# 3
+print(str(a)+'x'+str(b)+'=12')
+
+# 4
+s = 'value: 123'
+print(s[7:])
+
+# 5
+s2 = 'I like dog'
+s3 = s2.replace('dog', 'cat')
+print(s3)
