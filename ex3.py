f76200a47a8a0d83f318c256735e62757190c6ba
hotehamahayato <hayato.hotehama@nds-osk.co.jp>
2025-05-16 13:24:13 +0900

    commit ex3.py

---
+++ ex3.py
@@ -0,0 +1,23 @@
+# 1
+a = [10, 8, 12, 17, 5]
+
+# 2
+print(a[-1])
+
+# 3
+a.append(13)
+print(a)
+
+# 4
+b = sorted(a)
+print(b)
+
+# 5
+d = {'dog':'犬', 'cat':'猫', 'bird':'鳥'}
+print(d)
+
+# 6
+print(d['cat'])
+
+# 7
+print('monkey' in d)
